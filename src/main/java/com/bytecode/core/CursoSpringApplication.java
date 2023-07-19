88aa31f3a2a776903718dc57698a3b0bc8f097f7
MPAux <mikepoweraux@gmail.com>
2023-07-19 12:37:16 +0200

    video 18

--- src/main/java/com/bytecode/core/CursoSpringApplication.java
+++ src/main/java/com/bytecode/core/CursoSpringApplication.java
@@ -24,10 +24,36 @@ public class CursoSpringApplication implements CommandLineRunner {
 	@Autowired
 	@Qualifier("serviceDecorado")
 	public PostService postService;
+
+	/*
+	@Autowired
+	@Qualifier("serviceDecorado")
+	public PostService postService;
+	
+	
+	 OTROS EJEMPLOS DE CÓMO INYECTAR
+	
+	
+	@Autowired
+	@Qualifier("serviceDecorado")
+	public void setPostService(PostService postService) {
+		this.postService = postService;
+		this.postService.addClass(CursoSpringApplication.class);
+	}
+	-------------------------------------
+	public CursoSpringApplication(@Qualifier("serviceDecorado") PostService postService) {
+		this.postService = postService;
+	}
+	
+	
+	*/
+	
+	
 	
 	public static void main(String[] args) {
 		SpringApplication.run(CursoSpringApplication.class, args);
 	}
+
 
 	@Override
 	public void run(String... args) throws Exception {
